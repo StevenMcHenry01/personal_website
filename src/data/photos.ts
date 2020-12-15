93f38eb967807c3c23eb4970255ef28c1e63c10f
Steven McHenry <stevenmchenry01@gmail.com>
2020-12-14 16:22:04 -0800

    added many more photos

--- src/data/photos.ts
+++ src/data/photos.ts
@@ -1,8 +1,8 @@
 export const photos = [
   {
     src: '/images/family/1.jpeg',
-    width: 3,
-    height: 4,
+    width: 5,
+    height: 3,
   },
   {
     src: '/images/family/2.jpeg',
@@ -11,8 +11,8 @@ export const photos = [
   },
   {
     src: '/images/family/3.jpeg',
-    width: 3,
-    height: 4,
+    width: 3.8,
+    height: 3,
   },
   {
     src: '/images/family/4.jpeg',
@@ -22,7 +22,7 @@ export const photos = [
   {
     src: '/images/family/5.jpeg',
     width: 3,
-    height: 4,
+    height: 5,
   },
   {
     src: '/images/family/6.jpeg',
@@ -31,7 +31,167 @@ export const photos = [
   },
   {
     src: '/images/family/7.jpeg',
-    width: 2,
+    width: 3,
+    height: 4,
+  },
+  {
+    src: '/images/family/8.jpeg',
+    width: 3,
+    height: 4,
+  },
+  {
+    src: '/images/family/9.jpeg',
+    width: 5,
+    height: 3.5,
+  },
+  {
+    src: '/images/family/10.jpeg',
+    width: 3,
+    height: 4,
+  },
+  {
+    src: '/images/family/11.jpeg',
+    width: 3,
+    height: 4,
+  },
+  {
+    src: '/images/family/12.jpeg',
+    width: 3,
+    height: 4,
+  },
+  {
+    src: '/images/family/13.jpeg',
+    width: 3,
+    height: 4,
+  },
+  {
+    src: '/images/family/14.jpeg',
+    width: 5,
+    height: 3.5,
+  },
+  {
+    src: '/images/family/15.jpeg',
+    width: 3,
+    height: 4,
+  },
+  {
+    src: '/images/family/16.jpeg',
+    width: 3,
+    height: 4,
+  },
+  {
+    src: '/images/family/17.jpeg',
+    width: 3,
+    height: 4,
+  },
+  {
+    src: '/images/family/18.jpeg',
+    width: 3,
+    height: 4,
+  },
+  {
+    src: '/images/family/19.jpeg',
+    width: 3,
+    height: 4,
+  },
+  {
+    src: '/images/family/20.jpeg',
+    width: 5,
+    height: 3.8,
+  },
+  {
+    src: '/images/family/21.jpeg',
+    width: 5.5,
+    height: 5,
+  },
+  {
+    src: '/images/family/22.jpeg',
+    width: 3,
+    height: 4,
+  },
+  {
+    src: '/images/family/23.jpeg',
+    width: 5,
+    height: 4,
+  },
+  {
+    src: '/images/family/24.jpeg',
+    width: 5,
+    height: 4,
+  },
+  {
+    src: '/images/family/25.jpeg',
+    width: 5,
+    height: 4,
+  },
+  {
+    src: '/images/family/26.jpeg',
+    width: 3,
+    height: 4,
+  },
+  {
+    src: '/images/family/27.jpeg',
+    width: 3.5,
+    height: 4,
+  },
+  {
+    src: '/images/family/28.jpeg',
+    width: 3,
+    height: 4,
+  },
+  {
+    src: '/images/family/29.jpeg',
+    width: 5,
+    height: 4,
+  },
+  {
+    src: '/images/family/30.jpeg',
+    width: 3,
+    height: 4,
+  },
+  {
+    src: '/images/family/31.jpeg',
+    width: 5,
+    height: 4,
+  },
+  {
+    src: '/images/family/32.jpeg',
+    width: 4,
+    height: 4,
+  },
+  {
+    src: '/images/family/33.jpeg',
+    width: 3,
+    height: 5,
+  },
+  {
+    src: '/images/family/34.jpeg',
+    width: 3,
+    height: 4,
+  },
+  {
+    src: '/images/family/35.jpeg',
+    width: 4,
+    height: 4,
+  },
+  {
+    src: '/images/family/36.jpeg',
+    width: 3,
+    height: 4,
+  },
+  {
+    src: '/images/family/37.jpeg',
+    width: 3,
+    height: 4,
+  },
+  {
+    src: '/images/family/38.jpeg',
+    width: 3,
+    height: 4,
+  },
+  {
+    src: '/images/family/39.jpeg',
+    width: 3,
     height: 4,
   },
 ]
